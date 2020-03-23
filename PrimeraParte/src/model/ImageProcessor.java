8fd3c7f71443987b2b7c0ec33f541d7663b7b1b4
njse22 <nicolas.salazar1@correo.icesi.edu.co>
2020-03-23 12:15:03 -0500

    nueva clase ImageProcessor, renombre de SingletonImageProcesor

--- PrimeraParte/src/model/ImageProcessor.java
+++ PrimeraParte/src/model/ImageProcessor.java
@@ -1,79 +1,57 @@
 package model;
 
+import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.image.PixelGrabber;
 import java.io.PrintWriter;
 
 public class ImageProcessor {
+	 
+	private Image image;
+	private String codecImage; 
+	private PixelGrabber grabber; 
 	
-	private static ImageProcessor instance = null; 
-	
-	private ImageProcessor() {}
-	
-	public synchronized static ImageProcessor getInstance() {
-		if(instance == null)
-			instance = new ImageProcessor(); 
-		return instance; 
+	public ImageProcessor() {
+		this.image = null; 
+		this.codecImage = ""; 
 	}
-
-	public void processImage(String infile) {
+	
+	public String getCodecImage() {
+		return codecImage;
+	}
+	
+	public void processImage(String infile)  {
 		infile = infile.toLowerCase();
-		java.awt.Image image = Toolkit.getDefaultToolkit().getImage(infile);
+		this.image = Toolkit.getDefaultToolkit().getImage(infile);
 
 		try {
 			
-			String outImage = ""; 
-			String outfile = infile.replace(".jpg", ".txt");
-			PrintWriter out = new PrintWriter(outfile);
-			PixelGrabber grabber = new PixelGrabber(image, 0, 0, -1, -1, false);
+			this.grabber = new PixelGrabber(image, 0, 0, -1, -1, false);
 
 			if (grabber.grabPixels()) {
 				int width = grabber.getWidth();
 				int heigth = grabber.getHeight();
 
 				int[] data = (int[]) grabber.getPixels();
-				int loopstatus = 1;
 				int output;
 
-				// default = 12500000.threshold value = 0 -> 99999999 (**currently not sure about the highest value...). **tips: adjust for every million first((+-)10000000)
-				int threshold = 12500000;
-
-				for (int i = 0; i < width * heigth; i++) {
-					// white
-					if (data[i] == 16777215) {
-						output = 1;
-					}
-					// black
-					else if (data[i] == 0) {
-						output = 0;
-					}
-					// value that are not white/black.
-					else if (data[i] < threshold) {
-						output = 0;
-					} else {
-						output = 1;
-					}
-					
-					outImage += output; 
-				}
 				// RLE - 
-				int n =  outImage.length();
+				int n =  data.length;
 				for (int i = 0; i < n; i++) {
 					int count = 1; 
-					while(i < n-1 && outImage.charAt(i) == outImage.charAt(i+1)) {
+					while(i < n-1 && data[i] == data[i+1]) {
 						count++; 
 						i++; 
 					}
-					out.print(count);
-					out.print(outImage.charAt(i));
+					codecImage += count;
+					codecImage += data[i];
 				}
 			}
-			out.close();
-		} catch (Exception e) {
+		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-
 	}
+	
 	
 	
 	
